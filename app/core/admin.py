c98dad373ba5509d1ded91e972f3a6a30cf9bc3b
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2024-05-04 15:44:25 +0530

    Adding more fields to the custom user admin page.

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Django admin page customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from . import models
 
@@ -11,5 +12,30 @@ class UserAdmin(BaseUserAdmin):
     """Adding customization to the admin page."""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        # 1st section : Field columns
+        (None, {'fields': ('name', 'email', 'password')}),
+        # 2nd section : Field columns
+        (
+            _('Permissions'), {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        # 3rd section : Field columns
+        (
+            _('Important Dates'), {
+                'fields': (
+                    'last_login',
+                )
+            }
+        )
+    )
+
+    readonly_fields = ['last_login']
+
 
 admin.site.register(models.User, UserAdmin)
